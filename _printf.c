ae5fb90236354902ebfdfe441a20d19dfaf1cdbe
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-27 17:28:50 -0600

    printf

--- _printf.c
+++ _printf.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 int _printf(const char *format, ...) {
+    int count = 0;
+
     va_list args;
     va_start(args, format);
-
-    int count = 0;
 
     while (*format) {
         if (*format == '%') {
