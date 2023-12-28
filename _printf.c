6e2faa93a3bcd7d09e16873eb74611258255c9c6
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-28 11:42:05 -0600

    s

--- _printf.c
+++ _printf.c
@@ -15,30 +15,32 @@ int _printf(const char *format, ...) {
     while (format && *format) {
         if (*format == '%') {
             format++;
-            switch (*format) {
-                case 'c':
-                    {
-                        char ch = va_arg(args, int);
-                        count += write(1, &ch, 1);
-                    }
-                    break;
-                case 's':
-                    {
-                        const char* str = va_arg(args, const char*);
-                        if (str != NULL) {
-                            count += write(1, str, strlen(str));
-                        } else {
-                            count += write(1, "(null)", 6);
+            // Check if the character following % is a valid format specifier
+            if (*format == 'c' || *format == 's' || *format == '%') {
+                switch (*format) {
+                    case 'c':
+                        {
+                            char ch = va_arg(args, int);
+                            count += write(1, &ch, 1);
                         }
-                    }
-                    break;
-                case '%':
-                    count += write(1, "%", 1);
-                    break;
-                default:
-                    count += write(1, "%", 1);
-                    count += write(1, format, 1);
-                    break;
+                        break;
+                    case 's':
+                        {
+                            const char* str = va_arg(args, const char*);
+                            if (str != NULL) {
+                                count += write(1, str, strlen(str));
+                            } else {
+                                count += write(1, "(null)", 6);
+                            }
+                        }
+                        break;
+                    case '%':
+                        count += write(1, "%", 1);
+                        break;
+                }
+            } else {
+                count += write(1, "%", 1);
+                count += write(1, format, 1);
             }
         } else {
             count += write(1, format, 1);
