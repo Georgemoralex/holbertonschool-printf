6237981e11ce1a39719290b4cfd0ca78ed11d694
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-28 16:48:47 -0600

    format

--- _printf.c
+++ _printf.c
@@ -1,70 +1,71 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <string.h>
+
 /**
-*_printf - similar to printf function
-*Description: simulates printf function
-*@format: input to the printf function
-*Return: the number of characters printed (excluding the null byte)
-*/
+ * _printf - similar to printf function
+ * Description: simulates printf function
+ * @format: input to the printf function
+ * Return: the number of characters printed (excluding the null byte)
+ */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	char ch;
-	va_list args;
-	const char *str;
+    int count = 0;
+    char ch;
+    va_list args;
+    const char *str;
 
-	if (format == NULL)
-		return (-1);
-	va_start(args, format);
+    if (format == NULL)
+        return (-1);
 
-	while (format && *format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')
-			{
-				return (-1);
-				break;
-			}
-			switch (*format)
-			{
-				case 'c':
-					{
-						ch = va_arg(args, int);
-						count += write(1, &ch, 1);
-					}
-					break;
-				case 's':
-					{
-						str = va_arg(args, const char*);
-						if (str != NULL)
-						{
-							count += write(1, str, strlen(str));
-						}
-						else
-						{
-							count += write(1, "(null)", 6);
-						}
-					}
-					break;
-				case '%':
-					count += write(1, "%", 1);
-					break;
-				default:
-					count += write(1, "%", 1);
-					count += write(1, format, 1);
-					break;
-			}
-		}
-		else
-		{
-			count += write(1, format, 1);
-		}
+    va_start(args, format);
 
-		format++;
-	}
-	va_end(args);
-	return (count);
+    while (format && *format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+                return (-1);
+
+            switch (*format)
+            {
+                case 'c':
+                {
+                    ch = va_arg(args, int);
+                    count += write(1, &ch, 1);
+                }
+                break;
+                case 's':
+                {
+                    str = va_arg(args, const char*);
+                    if (str != NULL)
+                    {
+                        count += write(1, str, strlen(str));
+                    }
+                    else
+                    {
+                        count += write(1, "(null)", 6);
+                    }
+                }
+                break;
+                case '%':
+                    count += write(1, "%", 1);
+                    break;
+                default:
+                    count += write(1, "%", 1);
+                    count += write(1, format, 1);
+                    break;
+            }
+        }
+        else
+        {
+            count += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return (count);
 }
