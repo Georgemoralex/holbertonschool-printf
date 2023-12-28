78fd7763a8012c3bd39cdc1668829cb8190eef0c
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-27 21:10:26 -0600

    s

--- _printf.c
+++ _printf.c
@@ -4,8 +4,11 @@
 
 int _printf(const char *format, ...) {
     int count = 0;
-
     va_list args;
+
+	if (format == NULL) {
+		return 1;
+	}
     va_start(args, format);
 
     while (format && *format) {
