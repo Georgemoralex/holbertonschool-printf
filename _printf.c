89b7bee6d0bb27705d0c281bdacc725da5477f33
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-28 12:07:48 -0600

    s

--- _printf.c
+++ _printf.c
@@ -16,7 +16,8 @@ int _printf(const char *format, ...) {
         if (*format == '%') {
             format++;
             if (*format == '\0') {
-                break;
+                return -1;
+				break;
             }
             switch (*format) {
                 case 'c':
