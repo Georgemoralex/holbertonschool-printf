69bc48093c4b19b99d42aaf906c3326495564c8e
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-28 20:58:14 -0600

    s

--- main.h
+++ main.h
@@ -2,5 +2,8 @@
 #define PRINTF_H
 
 int _printf(const char *format, ...);
+void print_char(char ch, int *count);
+void print_string(const char *str, int *count);
+void print_number(int num, int *count);
 
 #endif
