1bb0ac73fe4d076cab58b1143e0c1d74d65ddd29
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-28 19:08:35 -0600

    s

--- _printf.c
+++ _printf.c
@@ -7,8 +7,7 @@
  * @ch: character to print
  * @count: pointer to the count of printed characters
  */
-void print_char(char ch, int *count)
-{
+void print_char(char ch, int *count) {
     *count += write(1, &ch, 1);
 }
 
@@ -17,8 +16,7 @@ void print_char(char ch, int *count)
  * @str: string to print
  * @count: pointer to the count of printed characters
  */
-void print_string(const char *str, int *count)
-{
+void print_string(const char *str, int *count) {
     if (str != NULL)
         *count += write(1, str, strlen(str));
     else
@@ -47,7 +45,7 @@ void print_number(int num, int *count) {
 
     while (divisor != 0) {
         digit = num / divisor;
-        *count += write(1, &digit, 1) + '0';
+        *count += write(1, &digit, 1);
         num %= divisor;
         divisor /= 10;
     }
@@ -59,8 +57,7 @@ void print_number(int num, int *count) {
  * @format: input to the printf function
  * Return: the number of characters printed (excluding the null byte)
  */
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
     int count = 0;
     va_list args;
 
