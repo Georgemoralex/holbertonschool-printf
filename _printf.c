55bfaaa8f7e272d4309bc7806ad6f507185eaabd
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2023-12-28 18:49:27 -0600

    s

--- _printf.c
+++ _printf.c
@@ -7,7 +7,8 @@
  * @ch: character to print
  * @count: pointer to the count of printed characters
  */
-void print_char(char ch, int *count) {
+void print_char(char ch, int *count)
+{
     *count += write(1, &ch, 1);
 }
 
@@ -16,7 +17,8 @@ void print_char(char ch, int *count) {
  * @str: string to print
  * @count: pointer to the count of printed characters
  */
-void print_string(const char *str, int *count) {
+void print_string(const char *str, int *count)
+{
     if (str != NULL)
         *count += write(1, str, strlen(str));
     else
@@ -28,7 +30,10 @@ void print_string(const char *str, int *count) {
  * @num: number to print
  * @count: pointer to the count of printed characters
  */
-void print_number(int num, int *count) {
+void print_number(int num, int *count)
+{
+    int divisor = 1;
+
     /* Handle negative numbers */
     if (num < 0) {
         *count += write(1, "-", 1);
@@ -36,7 +41,6 @@ void print_number(int num, int *count) {
     }
 
     /* Convert digits to characters and print */
-    int divisor = 1;
     while (num / divisor > 9) {
         divisor *= 10;
     }
@@ -56,7 +60,8 @@ void print_number(int num, int *count) {
  * @format: input to the printf function
  * Return: the number of characters printed (excluding the null byte)
  */
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     int count = 0;
     va_list args;
 
